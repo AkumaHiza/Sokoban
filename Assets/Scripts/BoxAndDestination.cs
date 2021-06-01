041fdc4b322befabe23497b40afee1da8ce4f7bd
Magdalena Rzepka <madzia.rzepka@vp.pl>
2021-06-01 12:30:49 +0200

    First commit

---
+++ Assets/Scripts/BoxAndDestination.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxAndDestination : MonoBehaviour
+{
+    private GameObject[] boxes;
+    private GameObject[] destinations;
+
+    //change color of box to green if box is on destination place
+    private void ColorBox(GameObject box)
+    {  
+      box.GetComponent<Renderer> ().material.color = new Color32(45,255,0,255);
+    }
+    private void UncolorBox(GameObject box)
+    {
+      box.GetComponent<Renderer> ().material.color = new Color32(255,255,255,255);   
+    }
+
+    // show "You win" caption
+    private void Finish()
+    {
+     GameObject win = GameObject.FindGameObjectWithTag("Win");
+     if(win.transform.localScale.x == 0) win.transform.localScale = new Vector3(6,6,0); 
+    }
+    void Start()
+    {
+     boxes        = GameObject.FindGameObjectsWithTag("Box");
+     destinations = GameObject.FindGameObjectsWithTag("Destination");
+    }
+
+    //check if boxes are on destination place
+    void Update()
+    {
+      int all = 0;
+      foreach(GameObject box in boxes)
+      {
+       int correct_place = 0;
+       foreach(GameObject dest in destinations)
+        {
+          if(box.transform.position == dest.transform.position)
+          {
+            correct_place = 1;
+            ColorBox(box);
+          }
+        }
+        if(correct_place == 0) UncolorBox(box);
+        else all++;
+      }
+      if(all == 6) Finish();
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+  public GameObject player;
+  public GameObject[] walls;
+  public GameObject[] boxes;
+  public Vector3 move;
+
+//Check if place is taken by wall or box
+public bool Taken(Vector3 place)
+{
+  foreach(GameObject wall in walls)
+    {
+      if(wall.transform.position == place) return true;
+    }
+  foreach(GameObject box in boxes)
+    {
+      if(box.transform.position == place) return true;
+    }
+  return false;
+}
+
+//Check if place is taken by box
+public bool IsBox(Vector3 place)
+{
+  foreach(GameObject box in boxes)
+    {
+      if(box.transform.position == place) return true;
+    }
+    return false;
+}
+public void MoveBox(Vector3 place, Vector3 vec)
+{
+  foreach(GameObject box in boxes)
+    {
+      if(box.transform.position == place) box.transform.position = box.transform.position + vec;
+    }
+}
+
+// If player can be move 'vec' and that place are empty - do it.
+// If position where player want to go is a box, and box can be moved - do it.
+// Box can be moved 'vec' if next place is empty.
+  public void CanMove(Vector3 vec)
+  {
+   if(Taken(player.transform.position + vec) == false)
+   {
+    player.transform.position = player.transform.position + vec;
+   }
+   else if(IsBox(player.transform.position + vec) == true)
+   {
+     if(Taken(player.transform.position + (2 * vec)) == false)
+     {
+       MoveBox(player.transform.position + vec, vec);
+       player.transform.position = player.transform.position + vec;
+     }
+   }
+  }
+
+    void Start()
+    {
+       player = GameObject.FindGameObjectWithTag("Player");
+       walls  = GameObject.FindGameObjectsWithTag("Wall");
+       boxes  = GameObject.FindGameObjectsWithTag("Box");
+       foreach(GameObject box in boxes) //Detaches the transform from its parent
+       {
+       box.transform.parent = null;
+       }
+    }
+   // If arrow key was pressed - move the character to the next field.
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+          {
+            move.Set(0.75f,0f,0f);
+          }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+          {
+            move.Set(-0.75f,0f,0f);  
+          }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+          {
+           move.Set(0f,0.75f,0f);
+          }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+          {
+           move.Set(0f,-0.75f,0f);
+          }
+          CanMove(move);
+          move.Set(0f,0f,0f);
+    }
+}
